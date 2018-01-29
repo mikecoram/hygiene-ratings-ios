961137b970b70860f839e5e4504ad0660a263650
mikecoram <mikecoram96@gmail.com>
2018-01-29 11:55:39 +0000

    Initial Commit

---
+++ Assignment2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Assignment2
+//
+//  Created by Mike Coram on 29/01/2018.
+//  Copyright © 2018 Mike Coram. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
